3b7639e5775c9da08d618ef6395174e596e6933e
rama <rama@javan.co.id>
2023-05-21 21:34:12 +0700

    wip

---
+++ app/routes/admin.jobs.tsx
@@ -0,0 +1,13 @@
+export default function () {
+  return (
+    <div className="relative mx-auto max-w-4xl md:px-8 xl:px-0">
+      <div className="pt-10 pb-16">
+        <div className="px-4 sm:px-6 md:px-0">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Admin: Jobs Page
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/routes/admin.users.tsx
@@ -0,0 +1,16 @@
+export default function () {
+  return (
+    <div className="relative mx-auto max-w-4xl md:px-8 xl:px-0">
+      <div className="pt-10 pb-16">
+        <div className="px-4 sm:px-6 md:px-0">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Admin: User Page
+          </h1>
+        </div>
+        <div className="px-4 sm:px-6 md:px-0">
+          <div className="py-6">{/* code */}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
